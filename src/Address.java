6954ef2dd868dca0454b38c748ebae30bb1c4eab
Parsa Shafagh Motlagh <parsa.shafagh.1382@gmail.com>
2022-11-12 10:16:29 +0330

    first commit

---
+++ src/Address.java
@@ -0,0 +1,11 @@
+public class Address{
+    //fields
+    int ziCcode;
+    String contry;
+    String city;
+    String email;
+
+    public void stringToString(){
+        System.out.println();
+    }
+}
